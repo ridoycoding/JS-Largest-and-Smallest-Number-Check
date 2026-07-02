43beb8686403f07a0c6335da7d4cd04ab1989f7a
RIFATUJJAMAN RIDOY <mk5122136@gmail.com>
2026-07-02 23:24:03 +0600

    Add files via upload

---
+++ Largest and Smallest Number/main.js
@@ -0,0 +1,29 @@
+let a = 200;
+let b = 10;
+let c = 320;
+
+
+if (a > b && a > c) {
+   console.log("Max number is : A")
+    if (b < c) {
+        console.log("Min number is : B")
+    } else {
+       console.log( "Min number is : C")
+    }
+} else if (b > a && b > c) {
+   console.log("Min number is : B")
+    if (a < c) {
+       console.log( "Min number is : A")
+    } else {
+        console.log( "Min number is : C")
+        
+        
+    }
+} else {
+    console.log("Max number is : C")
+    if (a < b) {
+       console.log("Min number is : A")
+    } else {
+      console.log( "Min number is : B")
+    }
+}
